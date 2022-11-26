5f03bc7917ae6005f98e4f69ab6f3eede3f5be1e
Lukas Lazarek <LukasALazarek@gmail.com>
2022-11-26 16:49:50 -0600

    Add info.rkt

---
+++ info.rkt
@@ -0,0 +1,11 @@
+#lang info
+
+(define collection "sgrep")
+(define build-deps '(;; "racket-doc"
+                     ;; "scribble-lib"
+                     "at-exp-lib"
+                     "rscript"))
+(define deps '("base"
+               "rscript"
+               "utill"))
+;; (define scribblings '(("scribblings/process-queue.scrbl")))
